2bd5ba76458cdff5ca900102408ce78e5e00ce00
zay <64491586+zaytiri@users.noreply.github.com>
2023-03-14 11:17:00 +0000

    add readme, changelog, setup.py, requirements

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import pathlib
+from configmanager.utils.progsettings import get_version
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+version = get_version()
+
+setup(
+    name="configmanager",
+    version=version,
+    description="python library to manage configurations from program arguments including doing commands and saving configurations in a yaml file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zaytiri/settings-manager",
+    project_urls={
+        'GitHub': 'https://github.com/zaytiri/settings-manager',
+        'Changelog': 'https://github.com/zaytiri/settings-manager/blob/main/CHANGELOG.md',
+    },
+    author="zaytiri",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="manager, configurations, settings, arguments, argparse, yaml, save",
+    package_data={'configmanager': ['progsettings.yaml']},
+    packages=["configmanager", "configmanager.configurations"],
+    python_requires=">=3.10.6",
+    install_requires=[
+      "PyYAML~=6.0"
+    ],
+    # entry_points={
+    #     "console_scripts": [
+    #         "configmanager=configmanager:app.main",
+    #     ],
+    # }
+)
